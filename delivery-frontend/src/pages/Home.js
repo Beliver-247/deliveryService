80b8fadc50b9cb75f59042bb951b18f4c784149b
Dumindu Mendis <121997251+Beliver-247@users.noreply.github.com>
2025-04-15 23:46:38 +0530

    removed unused components(update staus and delivery details.)

--- delivery-frontend/src/pages/Home.js
+++ delivery-frontend/src/pages/Home.js
@@ -1,13 +1,9 @@
 import CreateDeliveryForm from "../components/CreateDeliveryForm";
-import UpdateStatusForm from "../components/UpdateStatusForm";
-import DeliveryDetails from "../components/DeliveryDetails";
 
 export default function Home() {
   return (
     <div className="space-y-10">
       <CreateDeliveryForm />
-      <UpdateStatusForm />
-      <DeliveryDetails />
     </div>
   );
 }
